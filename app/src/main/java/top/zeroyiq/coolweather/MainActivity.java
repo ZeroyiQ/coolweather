312d33b12464d11f0e719ef20d8e12e73d554b77
ZeroyiQ <zeroyiq@qq.com>
2017-08-03 15:02:56 +0800

    First commit

---
+++ app/src/main/java/top/zeroyiq/coolweather/MainActivity.java
@@ -0,0 +1,13 @@
+package top.zeroyiq.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
